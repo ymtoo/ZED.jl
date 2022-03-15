a34b613599b8d195a99cb16bb02fcc57a81ff6ad
ymtoo <ymtoo86@gmail.com>
2022-03-15 10:41:24 +0800

    chore: add libsl_zed_c for aarch64

--- src/ZED.jl
+++ src/ZED.jl
@@ -16,7 +16,13 @@ include("retrieve.jl")
 const LIBDIR = joinpath(dirname(pathof(@__MODULE__)), "..", "lib")
 
 const zed = if Sys.islinux()
-                joinpath(LIBDIR, "Linux/libsl_zed_c.so")
+                if Sys.ARCH == :x86_64
+                    joinpath(LIBDIR, "Linux/x86_64/libsl_zed_c.so")
+                elseif Sys.ARCH == :aarch64
+                    joinpath(LIBDIR, "Linux/aarch64/libsl_zed_c.so")
+                else
+                    error("Not implemented")
+                end
             elseif Sys.iswindows()
                 # TODO
                 error("Not implemented")
